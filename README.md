a2c583202b5dc7d02afac21bee3781520522ab97
shreve <eric@shreve.dev>
2024-03-29 06:34:06 +0000

    docs: update readme and add comments

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # toolbox
+
+## Todo
+
+- [ ] Add `docker` role
+- [ ] Add `aliases` to .zshrc
+- [ ] Configure `charm` server to run via docker, back it up to NAS
+- [ ] Use `skate` to handle secrets
+- [ ] Bake all of this into a docker image
+
+## Usage
+
+```shell
+$ cd /path/to/toolbox
+$ ./install.sh
+$ cd ansible && ansible-playbook main.yml
+```
+
+## Ansible Roles
+
+### `term`
+
+Handles initial basic setup of terminal environment. The tasks in this role
+prepare to use ansible to install and configure the rest of the environment.
+
+- python3 and pip
+- ansible and ansible-lint
+- zsh and oh-my-zsh
+- zsh plugins; most importantly fzf and forgit
+
+### `go`
+
+- Go 1.21.8
+- A handful of go tools
--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update -y
 
-# Install python3 and ansible
+# Install python3
 sudo apt install \
     python3 \
     python3-pip \
     python3-apt
 
-# Install ansible
+# Install ansible via pip
 python3 -m pip install --upgrade pip
 pip install --user ansible ansible-lint
 
-# Install fzf dependencies
+# Install basic dependencies
 sudo apt install \
     git \
     fd-find \
@@ -21,7 +21,7 @@ sudo apt install \
     tmux
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
 
-# Check fzf installation
+# Check fzf installation, remove it if it exists
 if [ -d $HOME/.fzf ]; then
     echo "FZF is already installed. Uninstalling existing FZF..."
     $HOME/.fzf/uninstall
@@ -31,5 +31,12 @@ fi
 echo "Installing FZF from git..."
 git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 $HOME/.fzf/install
+
+# Run ansible playbook
+cd ansible
+ansible-playbook main.yml
+cd
+
+echo "Installation complete!"
 
 
