c02a8bcec6da4912a3527a04a9994bbdbea3cd54
shreve <eric@shreve.dev>
2024-03-29 00:11:52 +0000

    feat(ansible): basic ansible usage
    Note, this commit wasn't working but I made it to save my work.

--- install.sh
+++ install.sh
@@ -4,21 +4,32 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt update -y
 
 # Install python3 and ansible
-sudo apt install python3 python3-pip python3-apt
+sudo apt install \
+    python3 \
+    python3-pip \
+    python3-apt
+
+# Install ansible
 python3 -m pip install --upgrade pip
 pip install --user ansible ansible-lint
 
 # Install fzf dependencies
-sudo apt install fd-find ripgrep tmux
+sudo apt install \
+    git \
+    fd-find \
+    ripgrep \
+    tmux
 sudo ln -s /usr/bin/fdfind /usr/bin/fd
 
 # Check fzf installation
-if [ ! -d ~/.fzf ]; then
-    echo "FZF is not installed. Installing FZF..."
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install
-else
-    echo "FZF is already installed. Skipping..."
+if [ -d $HOME/.fzf ]; then
+    echo "FZF is already installed. Uninstalling existing FZF..."
+    $HOME/.fzf/uninstall
+    rm -rf $HOME/.fzf
 fi
+
+echo "Installing FZF from git..."
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
 
 
