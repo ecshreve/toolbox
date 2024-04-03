d045566efb1e1a3e96d9f055bd248805ba17f81e
shreve <eric@shreve.dev>
2024-04-03 08:15:05 +0000

    feat(llm): chat assistant config

--- assistant/client.py
+++ assistant/client.py
@@ -1,16 +1,27 @@
-import os
-import streamlit as st
-from langchain_openai import OpenAI
+from langchain.agents import ConversationalChatAgent, AgentExecutor
+from langchain.memory import ConversationBufferMemory
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.runnables import RunnableConfig
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain_community.document_loaders.git import GitLoader
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from langchain.tools.retriever import create_retriever_tool
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
 
+import streamlit as st
+
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-st.title("🦜🔗 Langchain Quickstart App")
-
+st.set_page_config(page_title="LangChain: Chat with search", page_icon="🦜")
+st.title("🦜 LangChain: Chat with search")
 with st.sidebar:
-    st.info('''Remember to set OPENAI_API_KEY
-            and/or other environment vars.
+    st.info('''Env vars:
             ''')
 
     # Check LANGCHAIN vars here too?
@@ -19,13 +30,63 @@ with st.sidebar:
     else:
         st.success("Environment variable are set correctly.")  
 
-def generate_response(input_text):
-    llm = OpenAI(temperature=0.7)
-    st.info(llm(input_text))
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(
+    chat_memory=msgs, return_messages=True, memory_key="chat_history", output_key="output"
+)
+if len(msgs.messages) == 0 or st.sidebar.button("Reset chat history"):
+    msgs.clear()
+    msgs.add_ai_message("How can I help you?")
+    st.session_state.steps = {}
+
+avatars = {"human": "user", "ai": "assistant"}
+for idx, msg in enumerate(msgs.messages):
+    with st.chat_message(avatars[msg.type]):
+        for step in st.session_state.steps.get(str(idx), []):
+            if step[0].tool == "_Exception":
+                continue
+            with st.status(f"**{step[0].tool}**: {step[0].tool_input}", state="complete"):
+                st.write(step[0].log)
+                st.write(step[1])
+        st.write(msg.content)
+
+search_tool = TavilySearchResults()
+
+loader = GitLoader(repo_path=os.getenv("TOOLBOX_PATH") or "../../", branch="main")
+docs = loader.load()
+documents = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).split_documents(docs)
+vector = FAISS.from_documents(documents, OpenAIEmbeddings(model="text-embedding-3-small"))
+retriever = vector.as_retriever()
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "retriever_tool",
+    "Search for information about system configuration and development environment setup."
+)
+
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wikipedia_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+
+tools = [search_tool, retriever_tool, wikipedia_tool]
 
 
-with st.form("my_form"):
-    text = st.text_area("Enter text:", "What are 3 key advice for learning how to code?")
-    submitted = st.form_submit_button("Submit")
-    if submitted:
-        generate_response(text)
+if prompt_str := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
+    st.chat_message("user").write(prompt_str)
+
+    llm = ChatOpenAI(model="gpt-4-turbo-preview", streaming=True)
+    chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=llm, tools=tools)
+    executor = AgentExecutor.from_agent_and_tools(
+        agent=chat_agent,
+        tools=tools,
+        memory=memory,
+        return_intermediate_steps=True,
+        handle_parsing_errors=True,
+    )
+
+    with st.chat_message("assistant"):
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        cfg = RunnableConfig()
+        cfg["callbacks"] = [st_cb]
+        response = executor.invoke({"input": prompt_str}, cfg)
+        st.write(response["output"])
+        st.session_state.steps[str(len(msgs.messages) - 1)] = response["intermediate_steps"]
