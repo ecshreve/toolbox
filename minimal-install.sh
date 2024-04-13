e58cb6790d0aae02653f4d0507604bbeb59a74e9
shreve <eric@shreve.dev>
2024-04-13 08:51:13 -0700

    feat: refactor setup flow

---
+++ minimal-install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+export TOOLBOX_DIR=$PWD
+export ANSIBLE_CONFIG=$TOOLBOX_DIR/ansible/ansible.cfg
+
+# check if ansible installed
+if [ -z "$(which ansible)" ]; then
+    echo "Ansible is not installed. Installing Ansible..."
+    sudo add-apt-repository ppa:ansible/ansible
+    sudo apt-get update -y && sudo apt-get install -y ansible
+else
+    echo "Ansible is already installed. Proceeding..."
+    echo $TOOLBOX_DIR
+    echo $ANSIBLE_CONFIG
+fi
+
+cd $TOOLBOX_DIR
+ansible-playbook site.yml -vv --check
+sudo chsh -s /usr/bin/zsh $USER
+
